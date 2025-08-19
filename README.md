c8a1e6ce1db1b904643351466d11b666c14491f1
Robert Stamate <rmstamate@gmail.com>
2025-08-19 12:05:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TheGuildProject
+
---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Assert for setup
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	bEnableTouchEvents = false;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent*  EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rot = GetControlRotation();
+	const FRotator YawRot(0.f, Rot.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRot).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRot).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
